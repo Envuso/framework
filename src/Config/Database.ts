daadb1b8295655ad75f4b728c1cdaf75e61bc280
Sam Parton <sam@idevelopthings.com>
2021-08-26 15:09:40 +0100

    Add seeder configuration/integration for next framework update
    Signed-off-by: Sam Parton <sam@idevelopthings.com>

--- src/Config/Database.ts
+++ src/Config/Database.ts
@@ -1,10 +1,22 @@
 import {MongoClientOptions} from "mongodb";
 import {ClientOpts} from "redis";
+import {Seeders} from "../Seeders/Seeders";
+import {DatabaseSeeder} from "@envuso/core/Database";
 
 export interface MongoConnectionConfiguration {
 	name: string;
 	url: string;
 	clientOptions: MongoClientOptions;
+}
+
+export interface RedisConnectionConfiguration extends ClientOpts {
+	enabled: boolean;
+}
+
+export interface DatabaseConfiguration {
+	mongo: MongoConnectionConfiguration;
+	redis: RedisConnectionConfiguration;
+	seeder: new () => DatabaseSeeder
 }
 
 export default {
@@ -18,12 +30,25 @@ export default {
 			useNewUrlParser    : true,
 			useUnifiedTopology : true
 		}
-	} as MongoConnectionConfiguration,
+	},
 
 	redis : {
+		/**
+		 * Set this to false to disable redis integration
+		 */
+		enabled : true,
+
 		prefix : 'envuso-',
 		//		db     : 'default',
-		host   : '127.0.0.1',
-		port   : 6379,
-	} as ClientOpts
-}
+		host : '127.0.0.1',
+		port : 6379,
+	},
+
+	/**
+	 * Your user defined seeder manager
+	 * This is where you will register all of your seeder instances
+	 * They will all be looped through and seeded.
+	 */
+	seeder : Seeders
+
+} as DatabaseConfiguration;
---
+++ src/Seeders/Seeders.ts
@@ -0,0 +1,9 @@
+import {DatabaseSeeder} from "@envuso/core/Database";
+
+export class Seeders extends DatabaseSeeder {
+
+	public registerSeeders() {
+		//this.add(UserSeeder);
+	}
+
+}
