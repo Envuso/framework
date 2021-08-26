0e662594d39e0fab4a85e6e17ac68f0e9b0f5988
Sam Parton <sam@idevelopthings.com>
2021-08-26 21:57:43 +0100

    Update storage configuration to new disks setup for
    Signed-off-by: Sam Parton <sam@idevelopthings.com>

--- src/Config/Storage.ts
+++ src/Config/Storage.ts
@@ -1,19 +1,23 @@
-import {S3Provider, StorageProviderContract} from "@envuso/core/Storage";
+import {LocalFileProvider, LocalStorageProviderConfiguration, S3Provider, S3StorageProviderConfiguration, StorageProviderContract} from "@envuso/core/Storage";
+import path from "path";
 
+export type DriverTypes = {
+	local: LocalStorageProviderConfiguration & { driver: 'local' },
+	s3: S3StorageProviderConfiguration & { driver: 's3' },
+};
 
-export type S3Config = {
-	bucket: string;
-	url: string;
-	endpoint: string;
-	credentials: {
-		accessKeyId: string;
-		secretAccessKey: string;
-	}
+export type DiskConfiguration<T extends keyof DriverTypes> = DriverTypes[T]
+
+export type DisksList = {
+	[key: string]: (DiskConfiguration<'local'> | DiskConfiguration<'s3'>)
 }
 
-export interface StorageConfig {
-	defaultProvider: new (storageConfig: StorageConfig) => StorageProviderContract;
-	s3: S3Config;
+export interface StorageConfiguration {
+	defaultDisk: keyof DisksList;
+	disks: DisksList;
+	drivers: {
+		[K in keyof DriverTypes]: new (storageConfig: DiskConfiguration<K>) => StorageProviderContract;
+	};
 }
 
 export default {
@@ -22,21 +26,36 @@ export default {
 	 * The default storage provider to use on the request() helper
 	 * or when using Storage.get(), Storage.put() etc
 	 */
-	defaultProvider : S3Provider,
+	defaultDisk : 'local',
 
-	/**
-	 * Your S3 config
-	 * (Should hopefully work for other services like DigitalOcean Spaces also)
-	 */
-	s3 : {
-		bucket      : process.env.SPACES_BUCKET,
-		url         : process.env.SPACES_URL,
-		endpoint    : process.env.SPACES_ENDPOINT,
-		credentials : {
-			accessKeyId     : process.env.SPACES_KEY,
-			secretAccessKey : process.env.SPACES_SECRET,
-		}
-	} as S3Config,
+	disks : {
+		s3      : {
+			driver      : 's3',
+			bucket      : process.env.SPACES_BUCKET,
+			url         : process.env.SPACES_URL,
+			endpoint    : process.env.SPACES_ENDPOINT,
+			credentials : {
+				accessKeyId     : process.env.SPACES_KEY,
+				secretAccessKey : process.env.SPACES_SECRET,
+			}
+		},
+		temp    : {
+			driver : 'local',
+			root   : path.join(process.cwd(), 'storage', 'temp'),
+		},
+		local   : {
+			driver : 'local',
+			root   : path.join(process.cwd(), 'storage', 'local'),
+		},
+		storage : {
+			driver : 'local',
+			root   : path.join(process.cwd(), 'storage'),
+		},
+	},
 
+	drivers : {
+		local : LocalFileProvider,
+		s3    : S3Provider,
+	}
 
-};
+} as StorageConfiguration;
