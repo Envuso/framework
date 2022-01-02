0ca055b459e1590d18e5e51636ed149c8218fdf6
Sam Parton <sam@idevelopthings.com>
2022-01-02 17:15:08 +0000

    Update for latest envuso version
    - Update example.env file to better match how the framework requires it to be
    - Update StaticAssetConfiguration.ts configuration - be sure to make sure your configuration matches
    - Update SessionConfiguration.ts configuration - remove dependency on "@envuso/core/Common/Utility/DateTime", this package was extracted
    
    Signed-off-by: Sam Parton <sam@idevelopthings.com>

--- src/Config/SessionConfiguration.ts
+++ src/Config/SessionConfiguration.ts
@@ -1,9 +1,8 @@
 import {ConfigurationCredentials} from "@envuso/core/AppContainer/Config/ConfigurationCredentials";
-import {DateTime} from "@envuso/core/Common/Utility/DateTime";
+import {DateTime} from "@envuso/date-time-helper";
 import {CookieConfiguration, SessionConfiguration as SessionConfig, SessionCookie} from "@envuso/core/Contracts/Session/Types";
 import {RedisSessionDriver} from "@envuso/core/Session/Drivers/RedisSessionDriver";
 import {SessionStorageDriver} from "@envuso/core/Session/Drivers/SessionStorageDriver";
-
 
 export class SessionConfiguration extends ConfigurationCredentials implements SessionConfig {
 
--- src/Config/StaticAssetConfiguration.ts
+++ src/Config/StaticAssetConfiguration.ts
@@ -1,6 +1,39 @@
-import * as path from "path";
 import {ConfigurationCredentials} from "@envuso/core/AppContainer";
+import {WatchOptions} from "chokidar";
 
 export default class StaticAssetConfiguration extends ConfigurationCredentials {
-	public assetsPath: string = path.join(process.cwd(), 'assets');
+
+	/**
+	 * This is the raw folder name in your application root directory
+	 *
+	 * For example, if we have our project at /EnvusoProject
+	 * Our asset's directory is registered as /EnvusoProject/assets
+	 *
+	 * @type {string}
+	 */
+	public assetsPath: string = 'assets';
+
+	/**
+	 * Essentially, by setting this to true, when our assets registered in our above asset's path
+	 * the server will be re-booted automatically.
+	 *
+	 * without this, the server won't have your routes/asset's registered if you were to
+	 * (for example) boot the server, then build your frontend asset's.
+	 *
+	 * By default, this is disabled so that you don't consume extra RAM in development
+	 * It will also be disabled in production(you shouldn't be using this in production)
+	 *
+	 *
+	 * @type {boolean}
+	 */
+	public watch: boolean = true;
+
+	/**
+	 * When the above "watch" option is set to true, we can define any options here which will be passed to chokidar
+	 *
+	 * @type {WatchOptions}
+	 */
+	public watchOptions: WatchOptions = {
+		ignoreInitial : true,
+	};
 }
